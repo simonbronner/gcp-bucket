d9c036915b818cc18cc3d658341819f1614ba176
Simon Bronner <simon.bronner@anz.com>
2020-07-31 11:57:31 +1000

    Added vault

--- __provider.tf
+++ __provider.tf
@@ -2,3 +2,18 @@ provider "google" {
   project     = var.project
   region      = var.region
 }
+
+provider "vault" {
+  address = "http://10.241.7.241:8200"
+}
+
+resource "vault_generic_secret" "example" {
+  path = "secret/foo"
+
+  data_json = <<EOT
+{
+  "foo":   "bar",
+  "pizza": "cheese"
+}
+EOT
+}
